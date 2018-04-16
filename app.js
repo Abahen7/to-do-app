0444e8d8628552955b99cea52b4efa45fc71884a
Alberto Bahena <2404albert@gmail.com>
2018-04-15 21:43:18 -0500

    Assignment 5 DOM 1: Redo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-  addToDoForm.addEventListener('submit', () => {
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     // get the text
     let title = newToDoText.value;
@@ -13,8 +13,14 @@ function onReady() {
     // create a new input
     let checkbox = document.createElement('input');
 
+    //create delete button
+    let deleteButton = document.createElement('button');
+
     // set the input's type to checkbox
     checkbox.type = "checkbox";
+
+    //add text to delete button
+    deleteButton.textContent = "Delete to-do";
 
     // set the title
     newLi.textContent = title;
@@ -22,11 +28,20 @@ function onReady() {
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    //attach the delete button to li
+    newLi.appendChild(deleteButton);
+
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
     //empty the input
     newToDoText.value = '';
+
+    //delete the to-do when the delete button is clicked
+    deleteButton.addEventListener('click', event => {
+      event.target.parentNode.remove();
+    });
+
   });
 }
 
