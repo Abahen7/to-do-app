2390c12cbbd56d1647699e010cc670817b898bf1
Alberto Bahena <2404albert@gmail.com>
2018-04-16 23:37:09 -0500

    Assignment 6 DOM 2: Completed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function onReady() {
+  let id = 0;
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
   function createNewToDo() {
@@ -6,21 +7,33 @@ function onReady() {
     if (!newToDoText.value) { return; }
     toDos.push({
       title: newToDoText.value,
-      complete: false
+			complete: false,
+			id: id++
     });
     newToDoText.value = '';
     renderTheUI();
   }
+  function deleteToDo () {
+		toDos.filter(toDos => toDos.id !== id.value);
+	}
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
     toDoList.textContent = '';
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const deleteButton = document.createElement('button');
       checkbox.type = "checkbox";
       newLi.textContent = toDo.title;
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+      deleteButton.addEventListener('click', event=>{
+			     event.preventDefault();
+			     event.target.parentNode.remove();
+			     rendertheUI();
+			})
+			deleteButton.textContent = "Delete";
     });
   }
   addToDoForm.addEventListener('submit', event => {
